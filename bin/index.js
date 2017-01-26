85895bf1e9bf53dda2fee688fad7164b47af1e13
Patrick Camacho <patrick.camacho@docker.com>
2017-01-26 14:29:02 -0800

    updated script to fail when process fails

--- bin/index.js
+++ bin/index.js
@@ -4,4 +4,12 @@
 var yarnOrNpm = require('../index');
 
 // Execute the command
-yarnOrNpm.spawn.sync(process.argv.slice(2), { stdio: 'inherit' });
+try {
+  const { status } = yarnOrNpm.spawn.sync(
+    process.argv.slice(2), { stdio: 'inherit' }
+  );
+  process.exit(status);
+} catch (err) {
+  console.log(err);
+  process.exit(1);
+}
