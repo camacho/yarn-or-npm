3a352b8f2d9795eebf2d106dada7cca026c42035
Nickolay Ribal <elektronik2k5@gmail.com>
2017-03-20 14:40:31 +0200

    Replace destructuring with regular property access

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,13 @@
 #! /usr/bin/env node
 'use strict';
 
-var yarnOrNpm = require('../index');
-
+const yarnOrNpm = require('../index');
 // Execute the command
 try {
-  const { status } = yarnOrNpm.spawn.sync(
-    process.argv.slice(2), { stdio: 'inherit' }
-  );
+  const status = yarnOrNpm.spawn.sync(
+    process.argv.slice(2),
+    { stdio: 'inherit' }
+  ).status
   process.exit(status);
 } catch (err) {
   console.log(err);
